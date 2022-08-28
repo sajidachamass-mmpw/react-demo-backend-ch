4d8e5a1d7e77372c14abc79ded31c670dd456cf3
Sajida Chamass <sajida.chamass@mmpww.com>
2022-08-28 10:39:42 +0300

    Add Roles

--- app/Http/Controllers/User/RoleController.php
+++ app/Http/Controllers/User/RoleController.php
@@ -3,15 +3,64 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RolePostRequest;
 use App\Models\User\Role;
 use App\Models\User\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\View;
+use App\Http\Resources\RoleCollection;
 
 class RoleController extends Controller
 {
-    public function index() {
-        $roles = Role::all();
-        return $roles;
+    public function index()
+    {
+        return RoleCollection::collection(Role::all());
     }
+
+    public function edit(Request $request)
+    {
+        return new RoleCollection(Role::findOrFail($request->get('id')));
+    }
+
+    public function updateRole(RolePostRequest $request)
+    {
+        $role = Role::find($request->get('id'));
+        $name = $request->get('name');
+
+        $validated = $request->validated();
+        if (!$validated) {
+            return 'error';
+        }
+
+        $role->name = $name;
+        $role->save();
+
+        return $role;
+    }
+
+    public function save(RolePostRequest $request)
+    {
+
+        $name = $request->get('name');
+        $validated = $request->validated();
+
+        if (!$validated) {
+            return 'error';
+        }
+
+        $role = new Role();
+        $role->name = $name;
+        $role->guard_name = 'web';
+        $role->save();
+
+        return $role;
+    }
+
+    public function destroy($id)
+    {
+        $role = Role::FindorFail($id);
+        $role->delete();
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/user', 'App\Http\Controllers\User\UserController@index');
-Route::get('/roles', 'App\Http\Controllers\User\RoleController@index');
+Route::get('/users', 'App\Http\Controllers\User\UserController@index');
 Route::post('/user/{id}/edit', 'App\Http\Controllers\User\UserController@edit');
 Route::post('/add-user', 'App\Http\Controllers\User\UserController@save');
 Route::post('/update-user', 'App\Http\Controllers\User\UserController@updateUser');
 Route::delete('/user/{id}', 'App\Http\Controllers\User\UserController@destroy');
+
+Route::get('/roles', 'App\Http\Controllers\User\RoleController@index');
+Route::post('/role/{id}/edit', 'App\Http\Controllers\User\RoleController@edit');
+Route::post('/update-role', 'App\Http\Controllers\User\RoleController@updateRole');
+Route::post('/add-role', 'App\Http\Controllers\User\RoleController@save');
+Route::delete('/role/{id}', 'App\Http\Controllers\User\RoleController@destroy');
